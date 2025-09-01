1e5e4356bcd8df97d98308c3994ffe86454d0347
Surjeet <surjeet@Surjeets-MacBook-Air.local>
2025-09-01 22:31:53 +0530

    add ec2 backend dns

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 
 
 // export const BASE_URL="https://social-media-plateform-vy0x.onrender.com"
-export const BASE_URL="http://ec2-18-209-102-96.compute-1.amazonaws.com"
+export const BASE_URL="http://ec2-34-228-223-161.compute-1.amazonaws.com:9090"
 
 // export const BASE_URL="http://localhost:9090"
 
