73f3ff1852332c38c74db8b511604fc7970b0e70
Surjeet <surjeet@Surjeets-MacBook-Air.local>
2025-09-02 14:08:56 +0530

    connect to render

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const { default: axios } = require("axios");
 
 
-// export const BASE_URL="https://social-media-plateform-vy0x.onrender.com"
-export const BASE_URL="http://ec2-34-228-223-161.compute-1.amazonaws.com:9090"
+export const BASE_URL="https://linkify-backend-7n20.onrender.com"
+// export const BASE_URL="http://ec2-34-228-223-161.compute-1.amazonaws.com:9090"
 
 // export const BASE_URL="http://localhost:9090"
 
